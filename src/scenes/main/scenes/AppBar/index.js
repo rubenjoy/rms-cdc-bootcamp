f498962a3ea1f726201e83a5ced880118ec44071
Yosua Simaremare <yosua161@gmail.com>
2017-05-04 16:58:23 +0800

    add AppBar scenes

---
+++ src/scenes/main/scenes/AppBar/index.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import {fade} from 'material-ui/utils/colorManipulator'
+import AppBar from 'material-ui/AppBar'
+import { Grid, Row, Col } from 'react-bootstrap'
+import './index.css' 
+
+import {
+    cyan500,
+    cyan700,
+    grey400,
+    pinkA200,
+    grey100,
+    grey500,
+    darkBlack,
+    white,
+    grey300,
+    fullBlack,
+    indigo500
+} from 'material-ui/styles/colors';
+
+const muiTheme = getMuiTheme({
+    palette: {
+        primary1Color: indigo500,
+        primary2Color: cyan700,
+        primary3Color: grey400,
+        accent1Color: pinkA200,
+        accent2Color: grey100,
+        accent3Color: grey500,
+        textColor: darkBlack,
+        alternateTextColor: white,
+        canvasColor: white,
+        borderColor: grey300,
+        disabledColor: fade(darkBlack, 0.3),
+        pickerHeaderColor: cyan500,
+        clockCircleColor: fade(darkBlack, 0.07),
+        shadowColor: fullBlack,
+    }
+}) 
+
+class App extends Component {
+    constructor () {
+        super(); 
+    }
+
+    render () { 
+
+        return (
+            <MuiThemeProvider muiTheme={muiTheme}>
+                <Grid fluid={true}>
+                <Row><AppBar id={'appss'}/></Row>
+                </Grid>
+            </MuiThemeProvider>
+        )
+    }
+
+}
+
+export default App;

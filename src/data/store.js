928092fe00cbf474260f6471fc24674ba380715e
Anggie Aziz <anggie.aziz@mitrais.com>
2017-05-10 12:39:11 +0700

    * Some little refactoring and store is now delivered on each related component ( use connect() ) * Routes is now better * Minor fixes

--- src/data/store.js
+++ src/data/store.js
@@ -5,12 +5,12 @@ import thunk from 'redux-thunk';
 // Middlewares
 const middlewares = [thunk];
 
+// For debug purpose (redux developer tool on chrome)
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // Store
 const reducers = combineReducers({
-    employees: employeesReducer.reducer,
-    currentEmployee: employeesReducer.currentEmployee
+    employees: employeesReducer.reducer
 });
 
 export default createStore(
